e3845717a7ad24451ab1630d9e0d29d8cd8b0bc5
Fellipe Augusto <Fellipe246@hotmail.com>
2023-02-13 20:23:24 -0300

    BootCamp

---
+++ src/Aula1.java
@@ -0,0 +1,13 @@
+public class Aula1 {
+    public static void main(String[] args) {
+        System.out.println("Primeira aula do bootcamp");
+
+        final int ano = 2023;
+        int ano2 = 1593;
+
+        //ano = 2159;
+        
+        
+    }
+    
+}
