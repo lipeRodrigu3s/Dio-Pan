a0f9a80bf862d566e5b84f8d4be20b409ab12982
Fellipe Augusto <Fellipe246@hotmail.com>
2023-02-17 15:15:49 -0300

    Bootcamp-DIO

---
+++ src/Aula2.java
@@ -0,0 +1,25 @@
+import javax.sound.midi.Soundbank;
+
+public class Aula2 {
+    public static void main(String[] args) {
+        
+
+        //String nome = "Fellipe";
+        //int anoNasci = 2000;
+        //boolean hoje = true;
+
+        //System.out.println(nome + anoNasci + hoje);
+        
+    int nasci = dataNascimento(19, 02, 2000);
+    System.out.println(nasci);
+
+    }
+
+    public static int dataNascimento(int dia, int mes, int ano){
+        return dia + mes + ano;
+
+    }
+
+    
+    
+}
