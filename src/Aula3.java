b60fd04b9ee9bee346f924a7a7c67ff26950af32
Fellipe Augusto <Fellipe246@hotmail.com>
2023-02-17 15:58:51 -0300

    BootcampDio

---
+++ src/Aula3.java
@@ -0,0 +1,15 @@
+public class Aula3 {
+    public static void main(String[] args) {
+
+        int n = 0;
+        n++;
+        System.out.println(n);
+
+        boolean v = true;
+        //v = !v;
+        System.out.println(v);
+
+
+
+    }
+}
