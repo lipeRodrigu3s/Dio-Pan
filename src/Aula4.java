c24e1681cfb2c5312b5a7f24bf009c59060bc601
Fellipe Augusto <Fellipe246@hotmail.com>
2023-02-18 11:40:29 -0300

    BootcampDio

---
+++ src/Aula4.java
@@ -0,0 +1,12 @@
+public class Aula4 {
+    public static void main(String[] args) {
+        int a,b;
+        a = 5;
+        b = 6;
+
+        String r =  a==b? "Sim":"Não";
+        System.out.println(r);
+
+
+    }
+}
