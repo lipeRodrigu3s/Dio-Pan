e4c53c9367493cbd2da4841535d4a9c5f9e946fb
Fellipe Augusto <Fellipe246@hotmail.com>
2023-02-23 17:56:22 -0300

    BootcampDIO

---
+++ src/aboutMe.java
@@ -0,0 +1,15 @@
+public class aboutMe {
+    public static void main(String[] args) {
+        String nome = args [0];
+        String sobrenome = args [1];
+        int idade = Integer.valueOf(args[2]);
+        double altura = Double.valueOf(args[3]);
+
+        System.out.println("ola, meu nome é " + nome + " " + sobrenome + " tenho " + idade + " anos e minha altura é " + altura + "cm");
+
+
+        
+        
+    
+    }
+}
